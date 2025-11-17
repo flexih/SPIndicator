6ba3f3c62f86c4ea1875378d3ee0ce40ec0c9f82
Xinghua <flexih@gmail.com>
2025-11-17 16:37:05 +0800

    Change UIGlassEffect style from clear to regular

--- Sources/SPIndicator/SPIndicatorView.swift
+++ Sources/SPIndicator/SPIndicatorView.swift
@@ -75,7 +75,7 @@ open class SPIndicatorView: UIView {
     private lazy var backgroundView: UIVisualEffectView = {
         let view: UIVisualEffectView = {
             if #available(iOS 26.0, *) {
-                return UIVisualEffectView(effect: UIGlassEffect(style: .clear))
+                return UIVisualEffectView(effect: UIGlassEffect(style: .regular))
             } else if #available(iOS 13.0, *) {
                 return UIVisualEffectView(effect: UIBlurEffect(style: .systemThickMaterial))
             } else {
